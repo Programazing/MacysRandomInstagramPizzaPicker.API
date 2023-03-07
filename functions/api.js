a68a97c63ceb52d581029e85aaec4a62ef1d1623
Programazing <chris@thatamazingprogrammer.com>
2023-03-06 23:07:04 -0500

    Add Pizza API

---
+++ functions/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const { fetchPizzaItems } = require('./pizzas.js');
+
+const app = express();
+const router = express.Router();
+
+const menusInput = {
+    shortUrl: 'macy-s-place-pizzeria',
+    restaurantGuid: '59d7aefc-7ec7-42fa-9c9a-1cf6f7e572e0',
+    menuApi: 'DO'
+  };
+
+// Use body-parser middleware to parse incoming JSON requests
+router.use(bodyParser.json());
+
+// Serve the index.html file at the root path
+router.get('/', (req, res) => {
+    res.send('App is running..');
+});
+
+// Endpoint to fetch a random pizza item
+router.get('/api/pizza', async (req, res) => {
+  try {
+    const pizzaItems = await fetchPizzaItems(menusInput);
+    const randomPizza = pizzaItems[Math.floor(Math.random() * pizzaItems.length)];
+    res.json(randomPizza);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching pizza items');
+  }
+});
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
---
+++ functions/pizzas.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch');
+const { MENUS_QUERY } = require('./queries.js');
+
+async function fetchPizzaItems(menusInput) {
+  const url = 'https://ws-api.toasttab.com/consumer-app-bff/v1/graphql';
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: MENUS_QUERY, variables: { input: menusInput } }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  const menus = data.data.menusV3.menus;
+  const foodMenu = menus.find((menu) => menu.name === 'Food Menu');
+  if (!foodMenu) {
+    throw new Error('Food Menu not found');
+  }
+
+  const instagramPizzasGroup = foodMenu.groups.find((group) => group.name === 'Instagram Pizzas');
+  if (!instagramPizzasGroup) {
+    throw new Error('Instagram Pizzas group not found in Food Menu');
+  }
+
+const itemsToFilter = ['Macys Fish Fry Dinner'];
+const instagramPizzasItems = instagramPizzasGroup.items.filter((item) => !itemsToFilter.includes(item.name));
+
+const uniqueInstagramPizzasItems = instagramPizzasItems
+  .map((item) => ({ ...item, name: item.name.replace(/^Large\s/, '').replace(/^Small\s/, '') }))
+  .filter((item, index, arr) => arr.findIndex((i) => i.name === item.name) === index);
+
+const pizzaItems = uniqueInstagramPizzasItems.map(({ name, description, imageUrl }) => ({ name, description, imageUrl }));
+
+  return pizzaItems;
+}
+
+module.exports = { fetchPizzaItems };
